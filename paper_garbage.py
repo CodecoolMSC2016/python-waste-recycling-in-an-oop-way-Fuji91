bd4dd516eb68acdb559b12aba1e236a2a009b6f2
Fuji91 <highwaytohell91@gmail.com>
2016-12-24 00:44:25 +0100

    ready

--- paper_garbage.py
+++ paper_garbage.py
@@ -2,4 +2,12 @@ from garbage import Garbage
 
 
 class PaperGarbage(Garbage):
-    pass
+
+    def __init__(self, name, squeeze):
+        super(PaperGarbage, self).__init__(name)
+        self.is_squeezed = squeeze
+
+    def squeeze(self):
+        if self.is_squeezed is False:
+            self.is_squeezed = True
+            return self.is_squeezed
